099c3264c0e71609cf3e8bbb27908980269900bd
Raghav Misra <raghav.m2014@gmail.com>
2020-06-14 13:27:33 -0700

    Yo header looks nice!

--- components/AppHeader.vue
+++ components/AppHeader.vue
@@ -1,17 +1,73 @@
 <template>
-    <header>
-        Header?
-    </header>
+    <b-navbar>
+        <template slot="brand">
+            <b-navbar-item tag="nuxt-link" to="/">
+                <img id="logo"
+                    src="/images/logo.svg"
+                    alt="AmadorUAVs Logo"
+                >
+            </b-navbar-item>
+        </template>
+        <template slot="start">
+            <!-- Pages -->
+            <b-navbar-item tag="nuxt-link" to="/about">About</b-navbar-item>
+
+            <b-navbar-item tag="nuxt-link" to="/team">The Team</b-navbar-item>
+
+            <b-navbar-item tag="nuxt-link" to="/join">Join</b-navbar-item>
+
+            <b-navbar-dropdown label="Our UAVs">
+                <b-navbar-item tag="nuxt-link" to="/uavs/macron">Macron</b-navbar-item>
+                <b-navbar-item tag="nuxt-link" to="/uavs/micron">Micron</b-navbar-item>
+            </b-navbar-dropdown>
+
+            <b-navbar-dropdown label="Sponsorship Info">
+                <b-navbar-item tag="nuxt-link" to="/sponsor/join">
+                    Become a Sponsor
+                </b-navbar-item>
+                <b-navbar-item tag="nuxt-link" to="/sponsor/list">
+                    Our Current Sponsors
+                </b-navbar-item>
+                <b-navbar-item tag="nuxt-link" to="/sponsor/list">Progress</b-navbar-item>
+            </b-navbar-dropdown>
+
+            <!-- External Links -->
+            <b-navbar-item href="http://www.auvsi-suas.org/" target="_blank">
+                Competition
+            </b-navbar-item>
+
+            <b-navbar-item href="https://medium.com/@AmadorUAVs" target="_blank">
+                Blog
+            </b-navbar-item>
+
+            <b-navbar-item href="https://gitlab.com/avhs-suas" target="_blank">
+                Source
+            </b-navbar-item>
+        </template>
+    </b-navbar>
 </template>
 
-<script lang="ts">
-import Vue from 'vue';
+<style>
+nav {
+    padding-top: 1rem;
+    padding-bottom: 1rem;
+    border-bottom: #7957d5 1px solid;
+}
 
-export default Vue.extend({
-    
-});
-</script>
+nav .navbar-item {
+    font-size: 1.125rem;
+}
 
-<style scoped>
+nav .navbar-dropdown > .navbar-item {
+    font-size: 1rem;
+}
 
+nav .navbar-dropdown {
+    border: #7957d5 1px solid;
+    border-radius: 6px;
+}
+
+nav #logo {
+    max-height: 3.75rem;
+}
 </style>
