c77e6027edf18fb323a36cd998e9b7db0466e6ac
bbworld1 <vwangsf@gmail.com>
2020-09-01 17:05:56 -0700

    Swap sponsor modals for links

--- pages/sponsor/list.vue
+++ pages/sponsor/list.vue
@@ -3,9 +3,9 @@
         <Jumbotron image="var(--banner-gradient)">
             <h1 class="has-text-white is-size-1 has-text-weight-bold">Our Current Sponsors</h1>
             <p class="has-text-white is-size-4">
-                Many thanks to the organizations who have given 
+                Many thanks to the organizations who have given
                 monetary, component, or service donations to our club!
-                Also, special thanks to any individuals 
+                Also, special thanks to any individuals
                 who have donated to our group!
             </p>
         </Jumbotron>
@@ -13,13 +13,13 @@
         <div class="section has-text-centered">
 
             <div class="container">
-                <div class="card sponsor-card" style="border-radius: 10px" 
+                <div class="card sponsor-card" style="border-radius: 10px; cursor: pointer"
                     v-for="(sponsor, i) in sponsorImages" :key="i"
                     @click="showModal(sponsor)"
                 >
                     <div class="card-image">
-                        <img 
-                            class="sponsor-image" 
+                        <img
+                            class="sponsor-image"
                             :src="`/images/sponsor/list/${sponsor.toLowerCase().replace(/ /g, `_`)}.png`"
                         />
                     </div>
@@ -28,7 +28,7 @@
 
             <div v-if="sponsorImages.length === 0">
                 <h1 class="title">
-                    No available sponsors right now. 
+                    No available sponsors right now.
                 </h1>
                 <b-button tag="nuxt-link" to="/sponsor/become" type="is-success">
                     Be the first!
@@ -40,14 +40,14 @@
             <div class="card has-text-centered modal-container">
                 <h1 class="title is-2">{{ currentModal.title }}</h1>
                 <div class="d-flex align-items-center modal-body">
-                    <img 
-                        class="flex-1" 
-                        :src="currentModal.image || `https://picsum.photos/500/300`" 
+                    <img
+                        class="flex-1"
+                        :src="currentModal.image || `https://picsum.photos/500/300`"
                     />
                     <p class="flex-1 subtitle">{{ currentModal.description }}</p>
                 </div>
                 <br>
-                <b-button 
+                <b-button
                     type="is-info"
                     tag="a" target="_blank"
                     :href="currentModal.website"
@@ -78,7 +78,43 @@ export default Vue.extend({
             modalData: {
                 "CUAV": {
                     description: "Cool Sponsorable site"
-                }
+                },
+                "Computar": {
+                    website: "https://cuav.org"
+                },
+                "FastSigns": {
+                    website: "https://fastsigns.com"
+                },
+                "JLCPCB": {
+                    website: "https://jlcpcb.com"
+                },
+                "Pololu": {
+                    website: "https://pololu.com"
+                },
+                "PTSA": {
+                    website: "https://amadorptsa.com"
+                },
+                "RFDesign": {
+                    website: "https://rfdesign.com.au"
+                },
+                "Sensirion": {
+                    website: "https://sensirion.com"
+                },
+                "SimScale": {
+                    website: "https://simscale.com"
+                },
+                "SKB": {
+                    website: "https://skbcases.com"
+                },
+                "StarTech": {
+                    website: "https://startech.com"
+                },
+                "The Imaging Source": {
+                    website: "https://theimagingsource.com"
+                },
+                "Weller": {
+                    website: "https://weller-tools.com"
+                },
             },
             currentModal: {
                 title: null,
@@ -90,12 +126,13 @@ export default Vue.extend({
     },
     methods: {
         showModal(sponsorName) {
-            this.currentModal = {
-                ...this.modalData[sponsorName],
-                title: sponsorName
-            }
+            window.location.href = this.modalData[sponsorName].website;
+            // this.currentModal = {
+            //     ...this.modalData[sponsorName],
+            //     title: sponsorName
+            // }
 
-            this.isModalActive = true;
+            // this.isModalActive = true;
         }
     }
 });
@@ -107,7 +144,7 @@ export default Vue.extend({
     vertical-align: top;
     padding: 2rem;
     width: 33%;
-    margin: 1rem 0; 
+    margin: 1rem 0;
     border: none;
     box-shadow: none;
 }
