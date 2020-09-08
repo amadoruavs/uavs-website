2484d0af25f0ea716f38130b97d80ef56efa7165
bbworld1 <vwangsf@gmail.com>
2020-09-07 17:03:55 -0700

    Change date

--- pages/join.vue
+++ pages/join.vue
@@ -4,7 +4,7 @@
             <h1 class="is-size-1 has-text-weight-bold has-text-white">Join Us!</h1>
             <p class="is-size-4 has-text-white">
                 Fill out the form below to register. Entrance assignments will be sent
-                September 7th.
+                September 14th.
             </p>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScJa0P9qoE-xoXUGoLjRQUovpCyTNQa0Iurwy_P3BWI8Inzxg/viewform?embedded=true" style="height: 50vh; margin-top: 1rem" width="640" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </Jumbotron>
