bb8686044d335c4edc2df62b845884ecc796cfd5
bbworld1 <vwangsf@gmail.com>
2020-09-20 10:16:58 -0700

    Update with entrance assignments

--- pages/join.vue
+++ pages/join.vue
@@ -3,8 +3,17 @@
         <Jumbotron image="var(--banner-gradient)">
             <h1 class="is-size-1 has-text-weight-bold has-text-white">Join Us!</h1>
             <p class="is-size-4 has-text-white">
-                Fill out the form below to register. Entrance assignments will be sent
-                September 17th. Email questions to officers@amadoruavs.com.
+                Entrance assignments are now open! Follow the instructions
+                to submit an entrance assignment below:
+            </p>
+            <p class="is-size-4">
+                <a style="text-decoration: underline" class="has-text-white" href="https://docs.google.com/document/d/1DMd6m-9b05RYZtv_gjGA1WJ8FKE5uHFXFuKMn6bmIbE/edit?usp=sharing">Software</a>
+            </p>
+            <p class="is-size-4">
+                <a style="text-decoration: underline" class="has-text-white" href="https://docs.google.com/document/d/1M2tfB-rnXl2pz3dF1Jb9nGWlQwKZY3MSV_64mvNG4s4/edit?usp=sharing">Electromechanical</a>
+            </p>
+            <p class="is-size-4">
+                <a style="text-decoration: underline" class="has-text-white" href="https://docs.google.com/document/d/1np6NizF01Dee2I7scfGZO5_PpjANkJP7v5vnjKLRVB0/edit?usp=sharing">Business</a>
             </p>
             <iframe src="https://docs.google.com/forms/d/e/1FAIpQLScJa0P9qoE-xoXUGoLjRQUovpCyTNQa0Iurwy_P3BWI8Inzxg/viewform?embedded=true" style="height: 50vh; margin-top: 1rem" width="840" frameborder="0" marginheight="0" marginwidth="0">Loading…</iframe>
         </Jumbotron>
