ea5439726935f525dc992eedae580529e7a2d8eb
bbworld <vwangsf@gmail.com>
2021-04-03 14:16:01 -0700

    Change scaling

--- pages/avfootball.vue
+++ pages/avfootball.vue
@@ -5,8 +5,8 @@
         Our club does livestreams of each football
         game our sports teams participate in. Watch here:
       </p>
-      <iframe style="width: 560px; height: 315px;" src="https://www.youtube.com/embed/e_TaEHBrGaw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
-      <p style="margin-top: 1rem">
+      <iframe style="width: 80vw; height: 45vw;" src="https://www.youtube.com/embed/e_TaEHBrGaw" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
+      <p style="margin-top: 1rem; margin-bottom: 2rem;">
         Older livestreams can be found archived
         <a href="https://www.youtube.com/watch?v=CaEpGgRDLN8">here.</a>
       </p>
