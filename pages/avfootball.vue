b2d58004b7b29dbbfefb2cf4cf98d466442e2f85
Angad-The-Coder <angad.bhargav@gmail.com>
2021-04-16 21:34:44 -0700

    Adding Junior Varsity game

--- pages/avfootball.vue
+++ pages/avfootball.vue
@@ -1,35 +1,63 @@
 <template>
-    <section id="avfootballPage" class="has-text-centered" style="padding-top: 2rem">
-      <h1 class="title">AV Football Streaming</h1>
-      <p style="margin-bottom: 0.5rem">
-        AmadorUAVs does livestreams of each football
-        game our sports teams participate in. Watch here:
-      </p>
-     <p class="subtitle"><b>Varsity Game</b></p>
-     <div style="display: flex; justify-content: center;">
-      <div style="width: 64vw; height: 36vw; border: 2px solid grey; display: flex; justify-content: center; align-items: center;">
-        <h1 class="subtitle">Unfortunately, the varsity game that was scheduled to be streamed on April 10th at 7:00 PM has been cancelled due to COVID-19.
-            Previous streams of our football games can be viewed <a href="https://www.youtube.com/playlist?list=PLj2ZpiiZsNz7cdf2MSgIVmPIc0Hlr1WKB">here</a>.</h1>
-      </div>
-      </div>
-      <br>
-      <br>
-      <!-- <p class="subtitle"><b>Freshman Game</b></p>
+  <section
+    id="avfootballPage"
+    class="has-text-centered"
+    style="padding-top: 2rem"
+  >
+    <h1 class="title">AV Football Streaming</h1>
+    <p style="margin-bottom: 1rem">
+      AmadorUAVs does livestreams of each football game our sports teams
+      participate in. Watch here:
+    </p>
+    <p class="subtitle" style="margin-bottom: 0.5rem">
+      <b>Junior Varsity Game</b>
+    </p>
+    <p style="margin-bottom: 0.5rem">
+      The Amador Valley Dons will face the Foothill Falcons on Saturday, April
+      17th at 4:00 PM.
+    </p>
+    <div
+      style="display: flex; justify-content: center; background-color: black"
+    >
+      <iframe
+        style="width: 72vw; height: 40.5vw"
+        src="https://www.youtube.com/embed/msREd27YXXc"
+        title="YouTube video player"
+        frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture"
+        allowfullscreen
+      ></iframe>
+      <iframe
+        style="width: 20vw; height: 40.5vw"
+        allowfullscreen=""
+        frameborder="0"
+        src="https://www.youtube.com/live_chat?v=e_TaEHBrGaw&embed_domain=amadoruavs.com"
+      ></iframe
+      ><br />
+    </div>
+    <br />
+    <br />
+    <!-- <p class="subtitle"><b>Freshman Game</b></p>
       <iframe style="width: 80vw; height: 45vw;" src="https://www.youtube.com/embed/kscDNfLxWbk" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture" allowfullscreen></iframe>
       -->
-      <p style="margin-top: 1rem; margin-bottom: 2rem;">
-        Older livestreams can be found archived
-        <a href="https://www.youtube.com/playlist?list=PLj2ZpiiZsNz7cdf2MSgIVmPIc0Hlr1WKB">here.</a>
-      </p>
-      <p class ="subtitle"><b>Support Us</b></p>
-      <p>
-       It takes a lot to run our club and these livestreams. If you want to help support us, <a href="https://amadoruavs.com/sponsor/become">sponsor us</a> or mail a check to Amador Valley High School with "for AmadorUAVs" in the memo line.
-      </p> 
-      <br>
-      <br>
-    </section>
+    <p style="margin-top: 1rem; margin-bottom: 2rem">
+      Older livestreams can be found archived
+      <a
+        href="https://www.youtube.com/playlist?list=PLj2ZpiiZsNz7cdf2MSgIVmPIc0Hlr1WKB"
+        >here.</a
+      >
+    </p>
+    <p class="subtitle"><b>Support Us</b></p>
+    <p>
+      It takes a lot to run our club and these livestreams. If you want to help
+      support us,
+      <a href="https://amadoruavs.com/sponsor/become">sponsor us</a> or mail a
+      check to Amador Valley High School with "for AmadorUAVs" in the memo line.
+    </p>
+    <br />
+    <br />
+  </section>
 </template>
 <script>
-export default {
-}
+export default {};
 </script>
